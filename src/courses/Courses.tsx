c869eec240b0272d2f310355835a5da82ba6c6f1
trungne <nguyenquochoangtrung@gmail.com>
2022-02-18 23:29:09 +0700

    remove unused commitse

--- src/courses/Courses.tsx
+++ src/courses/Courses.tsx
@@ -1,9 +1,7 @@
 import communicativeImage from './static/communicative.png';
 import ieltsImage from './static/ielts.png';
 import Course from './Course'
-import Menu from '../menu/Menu';
 import styles from './course.module.css';
-import { Outlet } from 'react-router-dom';
 
 export default function Courses() {
     return (
--- src/courses/communicative/Communicative.tsx
+++ src/courses/communicative/Communicative.tsx
@@ -1,5 +1,3 @@
-import Menu from "../../menu/Menu";
-
 export default function Communicative(){
     return (
         <div>
