69b5900ca3cc095bc6a2867838e85cb724c57681
trungne <nguyenquochoangtrung@gmail.com>
2022-02-18 23:26:15 +0700

    fix typo

--- src/courses/Courses.tsx
+++ src/courses/Courses.tsx
@@ -1,6 +1,6 @@
 import communicativeImage from './static/communicative.png';
 import ieltsImage from './static/ielts.png';
-import Course from './course'
+import Course from './Course'
 import Menu from '../menu/Menu';
 import styles from './course.module.css';
 import { Outlet } from 'react-router-dom';
