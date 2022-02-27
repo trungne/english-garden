a458a2615b127b5294bc385be55c09edff694efd
trungne <nguyenquochoangtrung@gmail.com>
2022-02-27 12:32:26 +0700

    add cover

--- src/courses/communicative/Communicative.tsx
+++ src/courses/communicative/Communicative.tsx
@@ -4,29 +4,65 @@ import leafImg from "./static/leaf.jpg"
 import flowerImg from "./static/flower.jpg"
 import fruitImg from "./static/fruit.jpg"
 
+
+import Typography from '@mui/material/Typography';
+import { createTheme } from "@mui/system";
+import { ThemeProvider } from "@emotion/react";
+
+const theme = createTheme({
+    typography: {
+        name: {
+            color: "#581f7e",
+            fontFamily: "SansterdamScript"
+        }
+    }
+})
+
+declare module '@mui/material/styles' {
+    interface TypographyVariants {
+        name: React.CSSProperties;
+    }
+
+    // allow configuration using `createTheme`
+    interface TypographyVariantsOptions {
+        name?: React.CSSProperties;
+    }
+}
+
+// Update the Typography's variant prop options
+declare module '@mui/material/Typography' {
+    interface TypographyPropsVariantOverrides {
+        name: true;
+    }
+}
+
 interface CourseProps {
     name: string,
     photoUrl: string,
     description: string,
+    direction: string,
 }
-function Course({ name, photoUrl, description }: CourseProps) {
+function Course({ name, photoUrl, description, direction }: CourseProps) {
     const style: React.CSSProperties = {
         backgroundImage: `url(${photoUrl})`,
-        backgroundSize: "100% 100%",
-        backgroundRepeat: "no-repeat",
-        height: "100%",
-        width: "100%",
     }
+    
+    const contentStyle: React.CSSProperties = {};
+    contentStyle[direction === "right" ? "marginRight" : "marginLeft"] = " 50%";
+
     return (
-        <div className={styles['course']}>
-            <div style={style} className={styles['course-image']}>
-
-            </div>
-
-            <div className={styles['course-content']}>
-                {description}
+        <div className={styles['course-overlay']}>
+            <div style={style} className={styles['course']}>
+                <div style={contentStyle} className={styles['course-content']}>
+                    <ThemeProvider theme={theme}>
+                        <Typography fontSize={"5vmin"} variant="name">
+                            {name}
+                        </Typography>
+                    </ThemeProvider>
+                </div>
             </div>
         </div>
+
     )
 }
 
@@ -35,29 +71,35 @@ const courses: CourseProps[] = [
         name: "Seed",
         photoUrl: seedImg,
         description: "Beginner Course",
+        direction: "left"
     },
     {
         name: "Leaf",
         photoUrl: leafImg,
         description: "Pre-intermediate Course",
+        direction: "right"
     },
     {
         name: "Flower",
         photoUrl: flowerImg,
         description: "Intermediate Course",
+        direction: "left"
     },
     {
         name: "Fruit",
         photoUrl: fruitImg,
         description: "Advanced Course",
+        direction: "right"
     },
 ]
 
 export default function Communicative() {
+
     return (
         <div className={styles['communicative']}>
             {courses.map(course => {
-                return <Course name={course.name} photoUrl={course.photoUrl} description={course.description} />
+
+                return <Course direction={course.direction} name={course.name} photoUrl={course.photoUrl} description={course.description} />
             })}
         </div>
     )
