1edef23762bd9ec61d585f5f9bfca4a539589023
trungne <nguyenquochoangtrung@gmail.com>
2022-03-01 02:07:00 +0700

    add effect to cover

--- src/courses/communicative/CourseInfo.tsx
+++ src/courses/communicative/CourseInfo.tsx
@@ -1,5 +1,15 @@
 import styles from "./communicative.module.css";
 
+import seedImg from "./static/seed.jpg"
+import leafImg from "./static/leaf.jpg"
+import flowerImg from "./static/flower.jpg"
+import fruitImg from "./static/fruit.jpg"
+
+import seedPreview1 from './static/seed_preview1.jpg';
+import seedPreview2 from './static/seed_preview2.jpg';
+import seedPreview3 from './static/seed_preview3.jpg';
+
+
 import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import Box from '@mui/material/Box';
@@ -72,23 +82,76 @@ function CourseOverviewItem({ text }: {
 
     )
 }
+
+const courses: CourseProps[] = [
+    {
+        course: {
+            name: "Seed",
+            photoUrl: seedImg,
+            description: "Hạt (Seed) là khóa học dành cho những ai vừa bắt đầu hành trình trồng cây. Tại Hạt, các bạn sẽ được giới thiệu và bồi đắp gốc rễ của tiếng Anh cũng như được thực hành với những tình huống giao tiếp thông dụng.",
+            duration: "120 hours",
+            level: "A1-A2",
+            previews: [seedPreview1, seedPreview2, seedPreview3],
+            outcomes: ["Giới thiệu bản thân và nói về sở thích",
+                "Hiểu và sử dụng những cụm từ thông dụng và cơ bản",
+                "Làm quen với những tình huống giao tiếp thông dụng",
+                "Sử dụng các thì cơ bản ở quá khứ, hiện tại, tương lai",
+                "Áp dụng tư duy phản biện",
+                "Sử dụng các từ nối cơ bản để nói và viết mạch lạc hơn",
+                "Phát âm cơ bản",
+                "Nhận biết các âm tiết dễ nhầm lẫn",
+                "Viết đoạn văn ngắn mô tả đồ vật và người",
+                "Áp dụng văn phong KISS (Keep It Short & Simple)"]
+        },
+    },
+    {
+        course: {
+            name: "Leaf",
+            photoUrl: leafImg,
+            description: "Pre-intermediate Course",
+            duration: "120 hours",
+            level: "B1",
+            previews: [],
+            outcomes: [],
+        },
+    },
+    {
+        course: {
+            name: "Flower",
+            photoUrl: flowerImg,
+            description: "Intermediate Course",
+            duration: "120 hours",
+            level: "B2",
+            previews: [],
+            outcomes: [],
+        },
+    },
+    {
+        course: {
+            name: "Fruit",
+            photoUrl: fruitImg,
+            description: "Advanced Course",
+            duration: "120 hours",
+            level: "C1",
+            previews: [],
+            outcomes: [],
+        },
+    },
+]
+
+
 export interface CourseProps {
     course: CommuncativeCourse,
-    direction: string,
-    coverImgUrl: string
 }
 
 export default function CourseInfo({
     course,
-    direction,
 }: CourseProps) {
 
     const coverStyle: React.CSSProperties = {
         backgroundImage: `url(${course.photoUrl})`,
     }
 
-    const titleStyle: React.CSSProperties = {};
-    titleStyle[direction === "right" ? "marginRight" : "marginLeft"] = " 50%";
 
     return (
         <div className={styles['course-overlay']}>
@@ -96,9 +159,9 @@ export default function CourseInfo({
                 triggerOnce={true}
                 damping={0.1}
                 cascade
-                direction={direction === "left" ? "left" : "right"}>
+                >
                 <div style={coverStyle} className={styles['course-banner']}>
-                    <div style={titleStyle} className={styles['course-title']}>
+                    <div className={styles['course-title']}>
                         <ThemeProvider theme={theme}>
                             <Typography fontSize={"10vmin"} variant="name">
                                 {course.name}
