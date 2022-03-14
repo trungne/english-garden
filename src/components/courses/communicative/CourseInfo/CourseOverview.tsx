7104c364c081ac58997fb9ec0e7807a61a56c81e
trungne <nguyenquochoangtrung@gmail.com>
2022-03-14 13:57:23 +0700

    style courseoverview

--- src/components/courses/communicative/CourseInfo/CourseOverview.tsx
+++ src/components/courses/communicative/CourseInfo/CourseOverview.tsx
@@ -2,20 +2,15 @@ import styles from "./courseinfo.module.css";
 import Paper from '@mui/material/Paper';
 import Typography from "@mui/material/Typography";
 import { Fade } from "react-awesome-reveal";
+import { Container } from "@mui/material";
+import Box from "@mui/system/Box";
 
 function OverviewItem({ content, backgroundColor }: { content: string, backgroundColor: string }) {
     return (
-        <Paper sx={{
-            display: "flex",
-            width: "100%",
-            maxWidth: "calc(1200px - 6em)",
-
-            alignItems: "center",
-            justifyContent: "center",
-            backgroundColor: backgroundColor,
-            flexGrow: '1',
+        <Paper className={styles['course-overview-item']} sx={{
+            backgroundColor: backgroundColor
         }}>
-            <Fade style={{ maxWidth: "600px" }}>
+            <Fade>
                 <Typography component={"div"} variant="normalText">
                     {content}
                 </Typography>
@@ -28,12 +23,12 @@ function OverviewItem({ content, backgroundColor }: { content: string, backgroun
 
 export default function CourseOverview({ duration, level }: { duration: number, level: string }) {
     return (
-        <div className={styles['course-overview']} >
-            <OverviewItem backgroundColor="#6db089" content={`Thời lượng: ${duration} tiếng`} />
-            <OverviewItem backgroundColor="#9ce0e3" content={`Trình độ đầu vào: ${level}`} />
-            <OverviewItem backgroundColor="#b1acde" content={`Học phí: 8.000.000 VNĐ`} />
-        </div >
-
-
+        <Container maxWidth='md' >
+            <Box className={styles['course-overview']}>
+                <OverviewItem backgroundColor="#6db089" content={`Thời lượng: ${duration} tiếng`} />
+                <OverviewItem backgroundColor="#9ce0e3" content={`Trình độ đầu vào: ${level}`} />
+                <OverviewItem backgroundColor="#b1acde" content={`Học phí: 8.000.000 VNĐ`} />
+            </Box>
+        </Container >
     )
 }
